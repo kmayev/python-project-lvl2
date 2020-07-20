68c9b3f08940f77b6b45a0ae629be27eb3ca9bb9
Konstantin Mayev <k.mayev@gmail.com>
2020-07-20 23:13:32 +0300

    20200720 First Commit

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,22 @@
+
+# -*- coding:utf-8 -*-
+
+
+import argparse
+
+
+def main():
+    """Run an example code."""
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
+
+if __name__ == '__main__':
+    main()
