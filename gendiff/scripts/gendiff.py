65723759215e51c60736af152a4e525221075b20
Konstantin Mayev <k.mayev@gmail.com>
2020-07-21 11:58:25 +0300

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,13 +7,22 @@ import argparse
 
 def main():
     """Run an example code."""
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
-
+    parser = argparse.ArgumentParser(description='Generate diff')
+#    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+#                    help='an integer for the accumulator')
+#    parser.add_argument('--sum', dest='accumulate', action='store_const',
+#                    const=sum, default=max,
+#                    help='sum the integers (default: find the max)')
+    """
+    parser.add_argument('--format', dest='', action='', nargs='?',
+                    default='plain',
+                    choices=['plain', 'pretty', 'JSON'],
+                    help='plain text | pretty | JSON')
+    """
+    parser.add_argument('first_file',
+                    type=argparse.FileType('r'))
+    parser.add_argument('second_file',
+                    type=argparse.FileType('r'))
     args = parser.parse_args()
     print(args.accumulate(args.integers))
 
